e0a5e6a81c6aa677fccfbd383ca4e418e435c578
subhasis2020 <54503787+subhasis2020@users.noreply.github.com>
2022-04-04 15:24:38 +0530

    Update ITetrisEngine.cs

--- Tetris/Service/TetrisEngine/ITetrisEngine.cs
+++ Tetris/Service/TetrisEngine/ITetrisEngine.cs
@@ -4,6 +4,11 @@ namespace Tetris.TetrisEngine
 {
     public interface ITetrisEngine
     {
+         // <summary>
+        /// initializes grid, changes the grid (drops all the shapes from one input line)
+        /// then checks if there is full-filled line on the grid and then removes it if so
+        /// </summary>
+        /// <returns></returns>
         Task<string> Play(string[] _inputData);
     }
 }
